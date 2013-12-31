14294374d585c85f5bad2f10b314ec3008e4d6bb
dborecki <borecki@cip.ifi.lmu.de>
2013-12-31 02:48:19 +0100

    SelectedNode-Label hinzugefügt.

--- FinalProject/src/DataTree.java
+++ FinalProject/src/DataTree.java
@@ -1,9 +1,8 @@
+import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Vector;
-
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTable;
@@ -13,64 +12,71 @@ import javax.swing.event.TreeSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.tree.DefaultMutableTreeNode;
 
-
-public class DataTree {
+public class DataTree implements TreeSelectionListener {
 
 	private JTree tree;
 	private DefaultMutableTreeNode root;
 	private DefaultTableModel default_table;
 
+	private JLabel selectedNode;
 	private JTable table;
 	private JSplitPane splitPane;
 	private JScrollPane treescroll, jtablescroll;
 
 	private Controller controller;
 
-	public DataTree(Controller controller){
+	public DataTree(Controller controller) {
 		this.controller = controller;
 		List<String> tables = this.controller.getTables("ALTDATEN");
 
-		/*Iterator<String> iterator = tables.iterator();
-		while(iterator.hasNext()){
-			System.out.println(iterator.next());
-		}*/
-		this.root = new DefaultMutableTreeNode("Datenbaum");
+		/*
+		 * Iterator<String> iterator = tables.iterator();
+		 * while(iterator.hasNext()){ System.out.println(iterator.next()); }
+		 */
+		this.root = new DefaultMutableTreeNode("ALTDATEN");
 		DefaultMutableTreeNode[] tableNodes = new DefaultMutableTreeNode[tables.size()];
 
 		int i = 0;
-		for(String table : tables){
+		for (String table : tables) {
 			tableNodes[i] = new DefaultMutableTreeNode(table);
-			List<String> columns = this.controller.getColumns("ALTDATEN", table);
-			for(String column : columns){
+			List<String> columns = this.controller
+					.getColumns("ALTDATEN", table);
+			for (String column : columns) {
 				tableNodes[i].add(new DefaultMutableTreeNode(column));
 			}
 			this.root.add(tableNodes[i]);
 			i++;
 		}
 
-
-
 		this.tree = new JTree(this.root);
 		this.tree.setRootVisible(false);
 		this.table = new JTable();
 		this.treescroll = new JScrollPane(this.tree);
-		this.jtablescroll = new JScrollPane(this.table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		this.splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, this.treescroll, this.jtablescroll);
+		this.selectedNode = new JLabel();
+		this.treescroll.add(this.selectedNode);
+
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(this.treescroll, BorderLayout.CENTER);
+		panel.add(this.selectedNode, BorderLayout.SOUTH);
+		this.jtablescroll = new JScrollPane(this.table,
+				JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		this.splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+				panel, this.jtablescroll);
 		this.splitPane.setOneTouchExpandable(true);
 		this.splitPane.setDividerLocation(200);
-		this.treescroll.setMinimumSize(new Dimension(100,50));
-		this.jtablescroll.setMinimumSize(new Dimension(100,50));
-		this.splitPane.setPreferredSize(new Dimension(400,200));
-		//this.tree.addTreeSelectionListener(this);
-
-
-		//DefaultMutableTreeNode node = (DefaultMutableTreeNode) this.tree.getLastSelectedPathComponent();
-		//if (node == null) return;
-		//String nodeInfo = (String) node.getUserObject();
-		//System.out.println(nodeInfo);
+		this.treescroll.setMinimumSize(new Dimension(100, 50));
+		this.jtablescroll.setMinimumSize(new Dimension(100, 400));
+		this.splitPane.setPreferredSize(new Dimension(400, 400));
+		this.tree.addTreeSelectionListener(this);
 	}
 
-	public JSplitPane getSplitPane(){
+	public JSplitPane getSplitPane() {
 		return this.splitPane;
+	}
+
+	@Override
+	public void valueChanged(TreeSelectionEvent e) {
+		this.selectedNode.setText(e.getPath().toString());
 	}
 }
