23ce71291b4b54550aaee8836d7eb1a31a7390c1
soni <dthsoni@live.com>
2014-01-17 08:07:19 +0100

    Bestell

--- FinalProject/src/gui/components/Bestellpositionen.java
+++ FinalProject/src/gui/components/Bestellpositionen.java
@@ -1,30 +1,29 @@
 package gui.components;
 
-import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Container;
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JSeparator;
 import javax.swing.JTextField;
+import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
-import javax.swing.SwingConstants;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.event.ListSelectionEvent;
@@ -36,9 +35,12 @@ public class Bestellpositionen extends JPanel implements Configuration,
 		ListSelectionListener {
 
 	private static final long serialVersionUID = -6721680340100703148L;
-
+	protected HashMap<String, Component> componentMap;
 	private JList<String> list;
 	private DefaultListModel<String> listModel;
+	private String tooltip;
+	private List<String> tooltips;
+	private int selectedIndex;
 
 	private static final String add = "Hinzufügen";
 	private static final String del = "Entfernen";
@@ -48,36 +50,27 @@ public class Bestellpositionen extends JPanel implements Configuration,
 
 	private JTextField txtInput;
 
-	public Bestellpositionen(String title) {
+	public Bestellpositionen(String name, String nameList, String nameAdd, String nameDel, String nameInp) {
 		super(new GridBagLayout());
+		this.setName(name);
+		setModel(null);
 
-		this.listModel = new DefaultListModel<String>();
-//		this.listModel.addElement("Test");
-
-		this.list = new JList<String>(this.listModel);
-		this.list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		this.list.setSelectedIndex(-1);
-		this.list.addListSelectionListener(this);
-		this.list.setVisibleRowCount(5);
-		this.list.setName(title);
-
-		JScrollPane listScrollPane = new JScrollPane(this.list);
 
 		this.btnAdd = new JButton(Bestellpositionen.add);
 		AddListener addListener = new AddListener(this.btnAdd);
 		this.btnAdd.addActionListener(addListener);
-		this.btnAdd.setName("add");
-		this.btnAdd.setActionCommand("add");
+		this.btnAdd.setName(nameAdd);
+		this.btnAdd.setActionCommand(nameAdd);
 		this.btnAdd.setEnabled(false);
 
 		this.btnDel = new JButton(Bestellpositionen.del);
-		this.btnDel.setName("del");
-		this.btnDel.setActionCommand("del");
+		this.btnDel.setName(nameDel);
+		this.btnDel.setActionCommand(nameDel);
 		this.btnDel.setEnabled(false);
 		this.btnDel.addActionListener(new DeleteListener());
 
 		this.txtInput = new JTextField(10);
-		this.txtInput.setName("bstPosInput");
+		this.txtInput.setName(nameInp);
 		this.txtInput.addActionListener(addListener);
 		this.txtInput.getDocument().addDocumentListener(addListener);
 //		String bestellPosString = this.listModel.getElementAt(this.list.getSelectedIndex()).toString();
@@ -94,12 +87,96 @@ public class Bestellpositionen extends JPanel implements Configuration,
 
 		addComponent(this, inputPanel, new Insets(0,0,0,0), 0, 0);
 		addComponent(this, buttonPanel, new Insets(0,0,0,0), 0, 1);
+		addListToPane(nameList, -1);
+		this.componentMap = new HashMap<String, Component>();
+		createComponentMap(this);
+	}
+
+	public JList getList(){
+		return this.list;
+	}
+
+	public DefaultListModel getListModel(){
+		return this.listModel;
+	}
+
+	public void setModel(DefaultListModel listModel){
+		if (listModel == null){
+			this.listModel = new DefaultListModel();
+		}
+		else {
+			this.listModel = listModel;
+		}
+	}
+
+	public void addModel(){
+		this.list.setModel(this.listModel);
+	}
+
+	public void removeList(){
+		this.remove(this.list);
+		this.remove(this.getComponentCount()-1);
+		this.revalidate();
+		this.repaint();
+	}
+
+	public void createComponentMap(Component component)
+	{
+		this.componentMap.put(component.getName(), component);
+		if (component instanceof Container)
+		{
+			if (((Container) component).getComponentCount() > 0){
+				for (Component child : ((Container) component).getComponents()){
+					createComponentMap(child);
+				}
+			}
+		}
+	}
+
+	public Component getComponentByName(String name) {
+		if (this.componentMap.containsKey(name)) {
+			return (Component) this.componentMap.get(name);
+		} else
+			return null;
+	}
+
+	public void addToolTips(List<String> tooltips){
+		this.tooltips = tooltips;
+	}
+
+	public List<String> getToolTips(){
+		return this.tooltips;
+	}
+
+	public void addListToPane(String nameList, int selectedIndex){
+		this.list = new JList<String>();
+		this.list.setModel(this.listModel);
+		this.list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		this.list.setSelectedIndex(selectedIndex);
+		this.list.addListSelectionListener(this);
+		this.list.setVisibleRowCount(5);
+		this.list.setName(nameList);
+		this.tooltips = new ArrayList<String>();
+		JScrollPane listScrollPane = new JScrollPane(this.list);
 		addComponent(this, listScrollPane, new Insets(0,0,0,0), 0, 2);
 	}
 
-	public int getList(){
-		return this.listModel.getSize();
+	public void addListener(){
+		this.list.addMouseMotionListener(new MouseMotionAdapter() {
+	        @Override
+	        public void mouseMoved(MouseEvent e) {
+	            JList l = (JList)e.getSource();
+	            ListModel m = l.getModel();
+	            int index = l.locationToIndex(e.getPoint());
+	            if( index>-1 ) {
+//	                l.setToolTipText("Preis: "+m.getElementAt(index).toString());
+	            	//tooltips.add(index, tooltip);
+	                l.setToolTipText(tooltips.get(index));
+	            }
+	        }
+	    });
 	}
+
 	public void addComponent(JPanel panel, Component c,
 			Insets insets, int x, int y) {
 		GridBagConstraints constraints = new GridBagConstraints();
@@ -160,7 +237,7 @@ public class Bestellpositionen extends JPanel implements Configuration,
 		public void actionPerformed(ActionEvent ae) {
 			String input = txtInput.getText();
 
-			if (input.equals("") || alreadyInList(input)) {
+			if (input.equals("") || alreadyInList(input) || !isValidInput(input)) {
 				Toolkit.getDefaultToolkit().beep();
 				txtInput.requestFocusInWindow();
 				txtInput.selectAll();
@@ -206,6 +283,12 @@ public class Bestellpositionen extends JPanel implements Configuration,
 			if (!alreadyEnabled) {
 				button.setEnabled(true);
 			}
+		}
+
+		private boolean isValidInput(String input){
+			input.split("\\;");
+			int elementCount = input.split("\\;").length;
+			return (elementCount <= 3 && elementCount > 1);
 		}
 
 		private boolean handleEmptyTextField(DocumentEvent de) {
